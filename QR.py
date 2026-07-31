1bfc1ab9a256c6cdcb2e6681b6efd1e326059ef9
Alhassan Faye <alhassan09@outlook.com>
2026-07-31 09:58:55 +0100

    Change character lookup to use ltAlternate
    Updated character lookup method to use ltAlternate list.

--- QR.py
+++ QR.py
@@ -2,6 +2,7 @@
 
 #things that never change
 lookup_table = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19, 'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35, ' ': 36, '$': 37, '%': 38, '*': 39, '+': 40, '-': 41, '.': 42, '/': 43, ':': 44}
+ltAlternate = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:")
 mode = "0010"
 
   #  if character not in lookup_table:
@@ -12,7 +13,7 @@ mode = "0010"
 input_text = input("Enter text>")
 digit_string = []
 for character in input_text:
-    digit = lookup_table[character]
+    digit = ltAlternate.index(character.upper())
     digit_string.append(digit)
 if len(digit_string) % 2 == 1: #if odd
     digit_string.append(36) #Adding an extra space for simplicity sake. I do not want to implement the rules for the last digit if the string is an odd number of characters.
@@ -52,3 +53,15 @@ while len(total_bit_stream) < 440:
     i += 1
 print(total_bit_stream)
 print(len(total_bit_stream))
+
+
+
+
+'''
+00100000 00110100 11110110 01100011 01110011 10001100 00000000 11101100
+00010001 11101100 00010001 11101100 00010001 11101100 00010001 11101100
+00010001 11101100 00010001 11101100 00010001 11101100 00010001 11101100
+00010001 11101100 00010001 11101100 00010001 11101100 00010001 11101100
+00010001 11101100 00010001 11101100 00010001 11101100 00010001 11101100
+00010001
+'''
